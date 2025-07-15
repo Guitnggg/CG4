455cf2851099368eaf5ee6ca2f346a7246d79423
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2025-07-15 14:01:48 +0900

    完成

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -69,7 +69,7 @@ void GameScene::Draw()
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 
-	model_->Draw(*worldTransform_, *camera_, textureHandle_);
+	//model_->Draw(*worldTransform_, *camera_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model2::PostDraw();
