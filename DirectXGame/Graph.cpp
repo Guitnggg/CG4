79bc0ce94bcf1c93e6c99b0d59128b3589cc304e
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2025-07-16 16:38:22 +0900

    表示サイズの変更

--- DirectXGame/Graph.cpp
+++ DirectXGame/Graph.cpp
@@ -18,14 +18,14 @@ void Graph::Initialize() {
     backBar_ = new Sprite();
     backBar_->Initialize();
     backBar_->SetPosition({ 100.0f, 50.0f });        // 表示位置
-    backBar_->SetSize({ 200.0f, 20.0f });            // サイズ（固定）
+    backBar_->SetSize({ 200.0f, 40.0f });            // サイズ（固定）
     backBar_->SetColor({ 1.0f, 0.0f, 0.0f, 1.0f });  // 赤
 
     // 表のグラフ
     frontBar_ = new Sprite();
     frontBar_->Initialize();
     frontBar_->SetPosition({ 100.0f, 50.0f });        // 表示位置（裏と同じ位置）
-    frontBar_->SetSize({ 200.0f, 20.0f });            // サイズ（初期サイズ）
+    frontBar_->SetSize({ 200.0f, 40.0f });            // サイズ（初期サイズ）
     frontBar_->SetColor({ 0.0f, 1.0f, 0.0f, 1.0f });  // 緑
 
     // 初期値を100%に設定
@@ -39,7 +39,7 @@ void Graph::SetValue(float rate) {
 
 void Graph::Update() {
     // 横幅を value_ に応じて変化させる
-    frontBar_->SetSize({ 200 * value_, 20 });
+    frontBar_->SetSize({ 200 * value_, 40 });
 }
 
 void Graph::Draw() {
